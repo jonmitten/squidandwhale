7d4fe19040c6f52866c777dd65b9e36bcdad88f5
jonmitten <jonmitten@gmail.com>
2021-08-31 14:46:59 -0700

    adds SQLAlchemy to requirements

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-import mysql.connector
 import json
-from flask import Flask
+import mysql.connector
+import re
+from flask import Flask, request
+from urllib.parse import unquote_plus
 
 app = Flask(__name__)
 
 
+def database_cursor():
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="p@ssw0rd1",
+    )
+    cursor = mydb.cursor()
+    return cursor
+
+def sum_record(a, b, sum):
+    cursor = database_cursor()
+    cursor.execute("INSERT INTO summation (a, b, c) VALUES ({a}, {b}, {c});".format(a=a, b=b, c=sum))
+    cursor.close()
 
 @app.route('/')
 def hello_world():
@@ -34,28 +49,58 @@ def get_widgets():
 
     return json.dumps(json_data)
 
+
+@app.route('/api/print', methods=['POST'])
+def print_test():
+    """
+    Send a POST request to localhost:5000/api/print with a JSON body with a "p" key
+    to print that message in the server console.
+    :return:
+    """
+    payload = request.get_json()
+    print(payload['p'])
+    return ("", 200, None)
+
+@app.route('/api/sum', methods=['POST'])
+def sum():
+    """
+    Send a POST request to localhost:5000/api/sum with a JSON body with an "a" and "b" key
+    to have the app add those numbers together and return a response string with their sum.
+    :return:
+    """
+    print("\nProcessing request")
+    print('\nrequest: ', request)
+    payload = request.get_json()
+    print("\nReceived following payload:")
+    print(payload)
+
+    print("\nAdding sum...")
+    summation = int(payload['a']) + int(payload['b'])
+    print("Found sum: {}".format(summation))
+    a = payload['a']
+    b = payload['b']
+    print("creating response string")
+    resp = '{a} + {b} = {sum}'.format(a=a, b=b, sum=summation)
+    print(resp)
+    sum_record(a=a, b=b, sum=summation)
+    return resp, 200, None
+
+
+
 @app.route('/initdb')
 def db_init():
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1",
-    )
-    cursor = mydb.cursor()
+    cursor = database_cursor()
 
     cursor.execute("DROP DATABASE IF EXISTS inventory")
     cursor.execute("CREATE DATABASE inventory")
     cursor.close()
 
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1",
-        database="inventory",
-    )
-    cursor = mydb.cursor()
-    cursor.execute("DROP TABLE IF EXISTS widgets")
-    cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
+    cursor = database_cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS widget")
+    cursor.execute("CREATE TABLE widget (name VARCHAR(255), description VARCHAR(255))")
+    cursor.execute("DROP TABLE IF EXISTS summation")
+    cursor.execute("CREATE TABLE summation (a INT, b INT, c INT)")
     cursor.close()
 
     return 'init database'
